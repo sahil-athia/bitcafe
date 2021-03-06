e18e3e871c6f6f308f8581b1ecc19be77bbd0350
Sahil Athia <sahilathia918@gmail.com>
2021-03-06 15:27:30 -0500

    add sliding menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Nav.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>BitCafe</h1>
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <ul id="menu">
+            <a href="#"><li>Home</li></a>
+            <a href="#"><li>About</li></a>
+            <a href="#"><li>Info</li></a>
+            <a href="#"><li>Contact</li></a>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
