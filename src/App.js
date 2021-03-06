537bcb9854199002869413d679275e57a2084cd3
Sahil Athia <sahilathia918@gmail.com>
2021-03-06 15:33:25 -0500

    move menu to component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/Nav.css'
-
+import './components/Menu.css'
+import Menu from './components/Menu.jsx'
 function App() {
   return (
     <div className="App">
       <h1>BitCafe</h1>
-      <nav role="navigation">
-        <div id="menuToggle">
-          <input type="checkbox" />
-
-          <span></span>
-          <span></span>
-          <span></span>
-
-          <ul id="menu">
-            <a href="#"><li>Home</li></a>
-            <a href="#"><li>About</li></a>
-            <a href="#"><li>Info</li></a>
-            <a href="#"><li>Contact</li></a>
-          </ul>
-        </div>
-      </nav>
+      <Menu />
     </div>
   );
 }
