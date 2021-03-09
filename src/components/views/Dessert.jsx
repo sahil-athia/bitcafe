5f730544b934fab3f6635c69b0f0afbdb0d0d40f
Sahil Athia <sahilathia918@gmail.com>
2021-03-09 14:14:24 -0500

    product divs

--- src/components/views/Dessert.jsx
+++ src/components/views/Dessert.jsx
@@ -1,8 +1,17 @@
+import "./product.scss"
+
 export default function Dessert() {
   // add left padding to accomodate menu
   return (
-    <div style={{marginTop: "400px"}}>
+    <div style={{marginTop: "100px"}} className="productPage">
       <h1>Show Desserts</h1>
+      <div className="productBox">
+
+      </div> 
+
+      <div className="productBox">
+
+      </div>
     </div>
   )
 }
